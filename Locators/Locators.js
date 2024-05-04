0d1474e3923e6915ec3a98a4ca43fbf59e957559
ViktorTester <viktor345@inbox.lv>
2024-05-04 21:51:09 +0300

    Locators_built_in.spec.js added

--- Locators/Locators.js
+++ Locators/Locators.js
@@ -1,5 +1,6 @@
 export const urls = {
-    url1: 'https://demoblaze.com'
+    url1: 'https://demoblaze.com',
+    url2: 'https://letcode.in/test'
 }
 
 export const demoblaze = {
@@ -9,4 +10,12 @@ export const demoblaze = {
     loginSubmitBtn: '//*[@id="logInModal"]/div/div/div[3]/button[2]',
     logoutSection: '//*[@id="logout2"]',
     allProductLinkElement: '.card-title > a'
+}
+
+export const letcode = {
+    inputName: 'Enter first & last name',
+    inputEditBtn: 'Edit',
+    editSectionBtn: ".card-footer > a",
+    allSectionBtns: '.card-footer-item',
+    newCourseBtn: 'div > .button.is-link'
 }
